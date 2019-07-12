4e8a93947505865ae64dd96f810a31942cdbd0ab
lefleur <lefleurlu@gmail.com>
2019-07-12 13:58:16 +0100

    Convert to MVC design pattern.

---
+++ todolist/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  todolist
+//
+//  Created by user154652 on 7/12/19.
+//  Copyright © 2019 lefleur. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var flag : Bool = false
+
+}
--- todolist/ToDoListViewController.swift
+++ todolist/ToDoListViewController.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    var itemArray = ["Eggs", "Bread", "Carrots"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let newItem = Item()
+        newItem.title = "Eggs"
+        itemArray.append(newItem)
+
+        let newItem2 = Item()
+        newItem2.title = "Carrots"
+        itemArray.append(newItem2)
+
+        let newItem3 = Item()
+        newItem3.title = "Bread"
+        itemArray.append(newItem3)
+
         // Do any additional setup after loading the view.
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
             itemArray = items
         }
         
@@ -35,7 +48,19 @@ class ToDoListViewController: UITableViewController {
         //let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        // Ternary operators ==>
+        // value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.flag ? .checkmark : .none
+//        same functionality
+//        if itemArray[indexPath.row].flag == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
         
         return cell
     }
@@ -43,12 +68,22 @@ class ToDoListViewController: UITableViewController {
     // TableView Delegate Methods for cell selection
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
+
+        itemArray[indexPath.row].flag = !itemArray[indexPath.row].flag
+//      same funcionalitu
+//        if itemArray[indexPath.row].flag == false {
+//            itemArray[indexPath.row].flag = true
+//        } else {
+//            itemArray[indexPath.row].flag = false
+//        }
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        tableView.reloadData()
+        
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        } else {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -62,8 +97,14 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what happens when user clicks the button on UIAlert
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            
             // forced unwrapped because the text property of a textfield will never be null
-            self.itemArray.append(textField.text!)
+            //self.itemArray.append(textField.text!)
             
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             
